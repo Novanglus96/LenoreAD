bc2bbed99613af82264f989429c03f1e8dc6dbf1
John Adams <johnmadams96@gmail.com>
2026-01-07 11:35:23 -0500

    chore: setup test/workspace environment

--- backend/core/services/active_directory.py
+++ backend/core/services/active_directory.py
@@ -1,10 +1,11 @@
 from ms_active_directory import ADDomain
 from django.conf import settings
 
+
 def get_ad_domain() -> ADDomain:
     return ADDomain(
         settings.AD_DOMAIN,
         settings.AD_USERNAME,
         settings.AD_PASSWORD,
-        ldap_server=settings.AD_DC
+        ldap_server=settings.AD_DC,
     )
